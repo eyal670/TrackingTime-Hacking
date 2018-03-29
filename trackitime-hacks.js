ea078f9d5f90d3cecfb9d8a0a05b991338b1b0a1
eyal670 <eyal670@gmail.com>
2018-03-29 14:55:07 +0300

    Update trackitime-hacks.js

--- trackitime-hacks.js
+++ trackitime-hacks.js
@@ -21,7 +21,7 @@ $( document ).ajaxStop(function(){
     if(!$('.jump-to-task').length){
       $('.task_name .title').map(function(){
          $(this).before('<a href="https://pro.trackingtime.co/#/project/'+$(this).closest('li').attr('data-project_id')+'/task/'+$(this).attr('data-taskid')+'" class="jump-to-task">⤻</a>');
-        $(this).before('<a href="https://pro.trackingtime.co/#/task/'+$(this).attr('data-taskid')+'" class="jump-to-task">🡭</a>');
+         //$(this).before('<a href="https://pro.trackingtime.co/#/task/'+$(this).attr('data-taskid')+'" class="jump-to-task">🡭</a>');
       });
     }
   }
